783004c5ae3a818d6f65560f9ccbabfa4b8cf264
xJetryyy <144103999+xJetryyy@users.noreply.github.com>
2024-08-25 16:30:28 +0300

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -9,10 +9,10 @@ const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_S': return 240; // 4 min 
-        case 'Length_M': return 300; // 5 min 
-        case 'Length_L': return 360; // 6 min 
-        case 'Length_XL': return 420; // 7 min 
+        case 'Length_S': return 240; // 4 min
+        case 'Length_M': return 300; // 5 min
+        case 'Length_L': return 360; // 6 min
+        case 'Length_XL': return 420; // 7 min
     }
-    return 300; 
+    return 300;
 }
